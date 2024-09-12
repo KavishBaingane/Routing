7d2a0124dee0286060ffaa84ecaccc4fc8e3cb68
Kavish-Evince <kavish@evincedev.com>
2024-09-12 14:25:02 +0530

    about routes-2
    master:routes-2

--- Routing/Program.cs
+++ Routing/Program.cs
@@ -23,17 +23,17 @@ app.UseEndpoints(endpoints =>
 
         await context.Response.WriteAsync($"In files {filename}-{extension}");
     });
-    endpoints.Map("employee/profile/{Employeename}", async
+    endpoints.Map("employee/profile/{Employeename:length(4,7):=kavish}", async
             context =>
     {
-            string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
-            await context.Response.WriteAsync($"In Employee profile {employeeName}");
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+        await context.Response.WriteAsync($"In Employee profile {employeeName}");
 
-        
-        
+
+
     });
     //Eg : product/details/1
-    endpoints.Map("products/details/{id?}", async
+    endpoints.Map("products/details/{id:int?}", async
         context =>
     {
         if (context.Request.RouteValues.ContainsKey("id"))
@@ -48,10 +48,41 @@ app.UseEndpoints(endpoints =>
 
         }
     });
+    //Eg daily-sigest/{reportDate}
+    endpoints.Map("daily-digest-report/{reportdate:datetime}", async
+        context =>
+    {
+
+        DateTime reportDate = Convert.ToDateTime(context.Request.RouteValues["reportdate"]);
+        await context.Response.WriteAsync($"In daily-digest-report --->{reportDate.ToShortDateString()}");
+
+    });
+
+    //Eg :cities/cityId 
+    endpoints.Map("cities/{cityid:guid}", async context =>
+    {
+        Guid cityId = Guid.Parse(Convert.ToString(context.Request.RouteValues["cityid"])!);
+        await context.Response.WriteAsync($"City Information - {cityId}");
+    });
+    //Sales report / 2024/ apr
+    endpoints.Map("sales-report/{year:int:min(1900)}/{month:regex(^(apr|oct|jan)$)}", async context =>
+    {
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = Convert.ToString(context.Request.RouteValues["month"]);
+        if(month=="apr"|| month=="jul" || month=="oct"|| month == "jan")
+        {
+            await context.Response.WriteAsync($"sales report - {year}-{month}");
+        }
+        else
+        {
+            await context.Response.WriteAsync($"{month} is not allowed");
+        }
+        
+    });
 });
 app.Run(async context =>
 {
-    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+    await context.Response.WriteAsync($"No route matched at {context.Request.Path}");
 });
 
 app.Run();
